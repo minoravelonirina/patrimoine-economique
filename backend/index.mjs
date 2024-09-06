696f03adcf4259c92bc4358ba9a9b4fc57179a37
minosoa <minoravelonirina@gmail.com>
2024-09-06 20:40:53 +0300

    feat: modify 2 files

--- backend/index.mjs
+++ backend/index.mjs
@@ -10,7 +10,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const PORT = process.env.PORT || 3005;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.json());
 app.use(express.static('../../ui/build'));
